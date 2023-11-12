7bda73c894458bdec52e3bc7320ff301b6db698b
TranMinhDuc <92939191+TranMinhDuc190103@users.noreply.github.com>
2023-11-12 14:10:55 +0700

    Add files via upload

--- 21002134_buoi5/main.py
+++ 21002134_buoi5/main.py
@@ -1,46 +1,20 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from database import *
-from database_model import *
 
 app = FastAPI()
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-def main(request: Request):
-    return templates.TemplateResponse("request.html", {"request": request})
-
-@app.post("/query", response_class=HTMLResponse)
-async def get_query(request: Request):
-    query = await request.form()
+async def main(request: Request, query: str):
     cursor.execute(query)
     result = cursor.fetchall()
-    # column_dict = {
-    #     "NHANVIEN": NHANVIEN_COLUMNS,
-    #     "CONGVIEC": CONGVIEC_COLUMNS,
-    #     "PHONGBAN": PHONGBAN_COLUMNS,
-    #     "PHANCONG": PHANCONG_COLUMNS,
-    #     "DEAN": DEAN_COLUMNS,
-    #     "THANNHAN": THANNHAN_COLUMNS,
-    #     "DIADIEM_PHG": DIADIEMPHG_COLUMNS
-    # }
-    if "NHANVIEN" in query:
-        columns = NHANVIEN_COLUMNS
-    elif "CONGVIEC" in query:
-        columns = CONGVIEC_COLUMNS
-    elif "PHONGBAN" in query:
-        columns = PHONGBAN_COLUMNS
-    elif "PHANCONG" in query:
-        columns = PHANCONG_COLUMNS
-    elif "DEAN" in query:
-        columns = DEAN_COLUMNS
-    elif "THANNHAN" in query:
-        columns = THANNHAN_COLUMNS
-    elif "DIADIEM_PHG" in query:
-        columns = DIADIEMPHG_COLUMNS
+    columns = [column[0] for column in cursor.description]
     return templates.TemplateResponse("result.html", {"request": request, "result": result, "columns": columns})
+
 
 if __name__ == "__main__":
     import uvicorn
